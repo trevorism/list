5a60ae22682301ff1923279c4ebdbcd31c77fb29
Trevor Brooks <trevorism@gmail.com>
2024-06-01 19:36:18 -0400

    Update dependencies

--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info
 @OpenAPIDefinition(
         info = @Info(
                 title = "List",
-                version = "0.4.0",
+                version = "0.5.0",
                 description = "API for management of lists within Trevorism",
                 contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
         )
