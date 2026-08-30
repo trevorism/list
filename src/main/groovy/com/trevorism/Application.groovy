e732cff860e8bc8b165b45631aac5a04b483e6ab
Trevor Brooks <trevorism@gmail.com>
2026-08-30 16:20:13 -0400

    fix version issue

--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info
 @OpenAPIDefinition(
         info = @Info(
                 title = "List",
-                version = "0.8.0",
+                version = AppVersion.SEMVER,
                 description = "API for management of lists within Trevorism",
                 contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
         )
